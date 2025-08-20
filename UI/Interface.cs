67010b9e8025a5fa0dad2bee7a1eabc455e61b31
Andre Jonas <nipsky@googlemail.com>
2025-08-20 15:23:24 +0200

    Working on interface

--- UI/Interface.cs
+++ UI/Interface.cs
@@ -4,154 +4,157 @@ using Godot;
 
 public partial class Interface : CanvasLayer
 {
-	// GamePanel GamePanel => GetNode<GamePanel>("%GamePanel");
-	// Control InterfacePanel => GetNode<Control>("%InterfacePanel");
-	// Control InterfaceContainer => InterfacePanel.GetNode<Control>("InterfaceContainer");
-	// Control InterfaceContainerDialog => InterfacePanel.GetNode<Control>("InterfaceContainerDialog");
+	GamePanel GamePanel => GetNode<GamePanel>("%GamePanel");
+	Control InterfacePanel => GetNode<Control>("%InterfacePanel");
+	Control InterfaceContainer => InterfacePanel.GetNode<Control>("InterfaceContainer");
+	Control InterfaceContainerDialog => InterfacePanel.GetNode<Control>("InterfaceContainerDialog");
 	// Control DialogOptionsContainer => InterfaceContainerDialog.GetNode<Control>("%DialogOptionsContainer");
-	// GridContainer InventoryGridContainer => InterfaceContainer.GetNode<GridContainer>("%Inventory");
-	// GridContainer VerbGridContainer => InterfaceContainer.GetNode<GridContainer>("%Verbs");
-	// Label CommandLabel => InterfaceContainer.GetNode<Label>("%CommandLabel");
+	GridContainer InventoryGridContainer => InterfaceContainer.GetNode<GridContainer>("%Inventory");
+	GridContainer VerbGridContainer => InterfaceContainer.GetNode<GridContainer>("%Verbs");
+	Label CommandLabel => InterfaceContainer.GetNode<Label>("%CommandLabel");
 
-	// [Signal] public delegate void GamePanelMouseMotionEventHandler();
-	// [Signal] public delegate void GamePanelMousePressedEventHandler(InputEventMouseButton mouseButtonEvent);
+	[Signal] public delegate void GamePanelMouseMotionEventHandler();
+	[Signal] public delegate void GamePanelMousePressedEventHandler(InputEventMouseButton mouseButtonEvent);
 	// [Signal] public delegate void ThingHoveredEventHandler(string thingID);
 	// [Signal] public delegate void ThingLeaveEventHandler();
 	// [Signal] public delegate void ThingClickedEventHandler(string thingID, Vector2 mousePosition);
-	// [Signal] public delegate void VerbHoveredEventHandler(string verbID);
-	// [Signal] public delegate void VerbLeaveEventHandler();
-	// [Signal] public delegate void VerbClickedEventHandler(string verbID);
+	[Signal] public delegate void VerbHoveredEventHandler(string verbID);
+	[Signal] public delegate void VerbLeaveEventHandler();
+	[Signal] public delegate void VerbClickedEventHandler(string verbID);
 	// [Signal] public delegate void DialogOptionClickedEventHandler(InkChoice choice);
+
+	[Export] public PackedScene VerbButtonScene;
+	[Export] public PackedScene InventoryButtonScene;
 
 	// // int Zoom = 4;
 
-	// enum MessageStateEnum
-	// {
-	// 	Idle,
-	// 	Keep
-	// }
+	enum MessageStateEnum
+	{
+		Idle,
+		Keep
+	}
 
-	// MessageStateEnum _messageState = MessageStateEnum.Idle;
-	// private MessageStateEnum MessageState
-	// {
-	// 	get => _messageState;
-	// 	set
-	// 	{
-	// 		if (value == MessageStateEnum.Keep)
-	// 			GetNode<Timer>("%MessageTimer").Start();
-	// 		_messageState = value;
-	// 	}
-	// }
+	MessageStateEnum _messageState = MessageStateEnum.Idle;
+	private MessageStateEnum MessageState
+	{
+		get => _messageState;
+		set
+		{
+			if (value == MessageStateEnum.Keep)
+				GetNode<Timer>("%MessageTimer").Start();
+			_messageState = value;
+		}
+	}
 
-	// public enum ModeEnum
-	// {
-	// 	Normal,
-	// 	Dialog
-	// }
+	public enum ModeEnum
+	{
+		Normal,
+		Dialog
+	}
 
-	// ModeEnum _mode = ModeEnum.Normal;
-	// public ModeEnum Mode
-	// {
-	// 	get => _mode;
-	// 	set
-    //     {
-    //         ModeChanged(value);
-    //         _mode = value;
-    //     }
-    // }
+	ModeEnum _mode = ModeEnum.Normal;
+	public ModeEnum Mode
+	{
+		get => _mode;
+		set
+		{
+			ModeChanged(value);
+			_mode = value;
+		}
+	}
 
-    // private void ModeChanged(ModeEnum value)
-    // {
-    //     switch (value)
-    //     {
-    //         case ModeEnum.Normal:
-    //             InterfaceContainer.Visible = true;
-    //             InterfaceContainerDialog.Visible = false;
-    //             break;
-    //         case ModeEnum.Dialog:
-    //             InterfaceContainer.Visible = false;
-    //             InterfaceContainerDialog.Visible = true;
-    //             break;
-    //     }
-    // }
+	private void ModeChanged(ModeEnum value)
+	{
+		switch (value)
+		{
+			case ModeEnum.Normal:
+				InterfaceContainer.Visible = true;
+				InterfaceContainerDialog.Visible = false;
+				break;
+			case ModeEnum.Dialog:
+				InterfaceContainer.Visible = false;
+				InterfaceContainerDialog.Visible = true;
+				break;
+		}
+	}
 
-    // public override void _Ready() => GamePanel.GuiInput += OnGamePanelInputEvent;
+	public override void _Ready() => GamePanel.GuiInput += OnGamePanelInputEvent;
 
-	// public void Init(Dictionary<string, string> verbs)
-	// {
-	// 	foreach (var verb in verbs)
-	// 	{
-	// 		var button = ResourceLoader.Load<PackedScene>("res://addons/GodotAdventureSystem/VerbButton.tscn").Instantiate() as Button;
-	// 		button.Text = verb.Value;
-	// 		button.MouseEntered += () => EmitSignal(SignalName.VerbHovered, verb.Key);
-	// 		button.MouseExited += () => EmitSignal(SignalName.VerbLeave);
-	// 		button.Pressed += () => EmitSignal(SignalName.VerbClicked, verb.Key);
-	// 		button.SetMeta("verb", verb.Key);
+	public void Init(Dictionary<string, string> verbs)
+	{
+		foreach (var verb in verbs)
+		{
+			var button = VerbButtonScene.Instantiate() as Button;
+			button.Text = verb.Value;
+			button.MouseEntered += () => EmitSignal(SignalName.VerbHovered, verb.Key);
+			button.MouseExited += () => EmitSignal(SignalName.VerbLeave);
+			button.Pressed += () => EmitSignal(SignalName.VerbClicked, verb.Key);
+			button.SetMeta("verb", verb.Key);
 
-	// 		VerbGridContainer.AddChild(button);
-	// 	}
+			VerbGridContainer.AddChild(button);
+		}
 
-	// 	var inventoryButtonCount = 8;
+		var inventoryButtonCount = 8;
 
-	// 	for (int i = 0; i < inventoryButtonCount; i++)
-	// 	{
-	// 		var button = ResourceLoader.Load<PackedScene>("res://addons/GodotAdventureSystem/InventoryButton.tscn").Instantiate() as InventoryButton;
-	// 		button.MouseEntered += () => OnInventoryButtonMouseEntered(button);
-	// 		button.MouseExited += OnInventoryButtonMouseExited;
-	// 		button.Pressed += () => OnInventoryButtonPressed(button);
-	// 		InventoryGridContainer.AddChild(button);
-	// 	}
-	// }
+		for (int i = 0; i < inventoryButtonCount; i++)
+		{
+			var button = InventoryButtonScene.Instantiate() as InventoryButton;
+			button.MouseEntered += () => OnInventoryButtonMouseEntered(button);
+			button.MouseExited += OnInventoryButtonMouseExited;
+			button.Pressed += () => OnInventoryButtonPressed(button);
+			InventoryGridContainer.AddChild(button);
+		}
+	}
 
-	// public void Reset()
-	// {
-	// 	ResetFocus();
-	// 	ClearDialogChoiceLabels();
-	// 	ClearInventory();
-	// }
+	public void Reset()
+	{
+		// ResetFocus();
+		ClearDialogChoiceLabels();
+		ClearInventory();
+	}
 
-	// public void ClearInventory()
-	// {
-	// 	foreach (var inventoryButton in InventoryGridContainer.GetChildren())
-	// 	{
-	// 		var button = inventoryButton as InventoryButton;
-	// 		button.SetThing(null);
-	// 	}
-	// }
+	public void ClearInventory()
+	{
+		foreach (var inventoryButton in InventoryGridContainer.GetChildren())
+		{
+			var button = inventoryButton as InventoryButton;
+			// button.SetThing(null);
+		}
+	}
 
-	// public void OnGamePanelInputEvent(InputEvent @event)
-	// {
-	// 	if (@event is InputEventMouseMotion mouseMotionEvent)
-	// 		EmitSignal(SignalName.GamePanelMouseMotion);
-	// 	else if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.Pressed)
-	// 		EmitSignal(SignalName.GamePanelMousePressed, mouseButtonEvent);
-	// }
+	public void OnGamePanelInputEvent(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion mouseMotionEvent)
+			EmitSignal(SignalName.GamePanelMouseMotion);
+		else if (@event is InputEventMouseButton mouseButtonEvent && mouseButtonEvent.Pressed)
+			EmitSignal(SignalName.GamePanelMousePressed, mouseButtonEvent);
+	}
 
-	// public void SetCommandLabel(string text, bool keep = false)
-	// {
-	// 	if (MessageState == MessageStateEnum.Idle)
-	// 	{
-	// 		CommandLabel.Text = text;
-	// 		// GD.Print($"SetCommandLabel: {text}");
+	public void SetCommandLabel(string text, bool keep = false)
+	{
+		if (MessageState == MessageStateEnum.Idle)
+		{
+			CommandLabel.Text = text;
+			// GD.Print($"SetCommandLabel: {text}");
 
-	// 		if (keep)
-	// 			MessageState = MessageStateEnum.Keep;
-	// 		// else
-	// 		// 	MessageState = MessageStateEnum.Idle;
-	// 	}
-	// }
+			if (keep)
+				MessageState = MessageStateEnum.Keep;
+			// else
+			// 	MessageState = MessageStateEnum.Idle;
+		}
+	}
 
-	// public void OnMessageTimerTimeout()
-	// {
-	// 	SetCommandLabel("");
-	// 	MessageState = MessageStateEnum.Idle;
-	// }
+	public void OnMessageTimerTimeout()
+	{
+		SetCommandLabel("");
+		MessageState = MessageStateEnum.Idle;
+	}
 
-	// public void ResetCommandLabel()
-	// {
-	// 	SetCommandLabel("");
-	// 	ResetFocus();
-	// }
+	public void ResetCommandLabel()
+	{
+		SetCommandLabel("");
+		// ResetFocus();
+	}
 
 	// public void OnPlayerObjectAddedToInventory(ThingResource thingResource)
 	// {
@@ -179,32 +182,30 @@ public partial class Interface : CanvasLayer
 	// 	}
 	// }
 
-	// public void OnInventoryButtonMouseEntered(InventoryButton inventoryButton)
-	// {
-	// 	var thingID = inventoryButton.GetMeta("thingID").AsString();
+	public void OnInventoryButtonMouseEntered(InventoryButton inventoryButton)
+	{
+		var thingID = inventoryButton.GetMeta("thingID").AsString();
 
-	// 	if (thingID != "")
-	// 	{
-	// 		// EmitSignal(SignalName.ThingClicked, thingID);
-	// 		EmitSignal(SignalName.ThingHovered, thingID);
-	// 	}
-	// }
+		if (thingID != "")
+		{
+			// EmitSignal(SignalName.ThingHovered, thingID);
+		}
+	}
 
-	// public void OnInventoryButtonMouseExited()
-	// {
-	// 	// ResetCommandLabel();
-	// 	EmitSignal(SignalName.ThingLeave);
-	// }
+	public void OnInventoryButtonMouseExited()
+	{
+		// EmitSignal(SignalName.ThingLeave);
+	}
 
-	// public void OnInventoryButtonPressed(InventoryButton inventoryButton)
-	// {
-	// 	var thingID = inventoryButton.GetMeta("thingID").AsString();
+	public void OnInventoryButtonPressed(InventoryButton inventoryButton)
+	{
+		var thingID = inventoryButton.GetMeta("thingID").AsString();
 
-	// 	if (thingID != "")
-	// 	{
-	// 		EmitSignal(SignalName.ThingClicked, thingID);
-	// 	}
-	// }
+		if (thingID != "")
+		{
+			// EmitSignal(SignalName.ThingClicked, thingID);
+		}
+	}
 
 	// public void ResetFocus()
 	// {
@@ -234,9 +235,9 @@ public partial class Interface : CanvasLayer
 	// 	}
 	// }
 
-	// public void ClearDialogChoiceLabels()
-	// {
-	// 	foreach (var child in DialogOptionsContainer.GetChildren())
-	// 		child.QueueFree();
-	// }
+	public void ClearDialogChoiceLabels()
+	{
+		// foreach (var child in DialogOptionsContainer.GetChildren())
+		// 	child.QueueFree();
+	}
 }
